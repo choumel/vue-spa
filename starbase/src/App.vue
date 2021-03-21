a306dae5c5c6961aa54fef962bcaca65f23f2849
Arnaud <choumelarnaud@gmail.com>
2021-03-21 21:42:38 +0100

    Single page application with VueJS

---
+++ starbase/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <h3> {{title}}</h3>
+        <character> </character>
+    </div>
+</template>
+
+import character from './components/character.vue'
+
+<script>
+import character from './components/character.vue'
+    export default {
+        name:'app',
+        data(){
+            return {
+                title: 'Generate Your Team'
+            }
+        },
+        components: {
+            character
+        }
+    }
+
+</script>
